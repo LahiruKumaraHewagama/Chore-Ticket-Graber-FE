7e67fc38f01f1c48a25ff6f51096df2490ae6fe4
Madhushani Hewagama <madhushani.hewagama@chakray.com>
2024-03-20 00:44:42 +0530

    added asgardieo

--- application/src/index.tsx
+++ application/src/index.tsx
@@ -1,26 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import * as authConfig from "./authconfig.json";
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+import { AuthProvider, useAuthContext } from "@asgardeo/auth-react";
+import React, { FunctionComponent, ReactElement } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./app.css";
+import { default as authConfig } from "./config.json";
+import { ErrorBoundary } from "./error-boundary";
+import NotFound from "./pages/not_found";
+import {LandingPage} from "./pages/landing_page";
+
+const AppContent: FunctionComponent = (): ReactElement => {
+    const { error } = useAuthContext();
+
+    return (
+        <ErrorBoundary error={error}>
+            <Router>
+            <Routes>
+                <Route path="/" element={ <LandingPage /> } />
+                <Route element={ <NotFound /> } />
+            </Routes>
+        </Router>
+        </ErrorBoundary>
+    )
+};
+
+const App = () => (
+    <AuthProvider config={authConfig}>
+        <AppContent />
+    </AuthProvider>
 );
 
-root.render(
-  // <AuthProvider config={{authConfig}}>
-  <React.StrictMode>
-    <div
-      style={{
-        backgroundImage: `url(${require("./resources/hotel-room.png")}`,
-        minHeight: "100vh",
-        backgroundSize: "cover",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <App />
-    </div>
-  </React.StrictMode>
-  // </AuthProvider>
-);
+render((<App />), document.getElementById("root"));
