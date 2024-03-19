eb850d3a5cccea6a3214f541d34c69d0b674627b
LahiruKumaraHewagama <lahiruhewagama98@gmail.com>
2024-03-19 17:17:23 +0530

    fix building failing

---
+++ application/src/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AuthProvider } from "@asgardeo/auth-react";
+import App from "./App";
+import * as authConfig from "./authconfig.json";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  // <AuthProvider config={{authConfig}}>
+  <React.StrictMode>
+    <div
+      style={{
+        backgroundImage: `url(${require("./resources/hotel-room.png")}`,
+        minHeight: "100vh",
+        backgroundSize: "cover",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <App />
+    </div>
+  </React.StrictMode>
+  // </AuthProvider>
+);
