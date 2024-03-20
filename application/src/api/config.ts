9d6b0edd881571c1b9cd97d2421f61d462ce3313
LahiruKumaraHewagama <lahiruhewagama98@gmail.com>
2024-03-20 14:30:11 +0530

    add pages to web

--- application/src/api/config.ts
+++ application/src/api/config.ts
@@ -7,4 +7,4 @@ declare global {
   }
 }
 
-export const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "http://localhost:4000/api/reservations";
+export const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "https://d4562274-bba9-41a5-be84-ebd80a7fac99-dev.e1-us-east-azure.choreoapis.dev/qtnh/hotel-reservations-api/hotel-reservation-api-0e0/v1.0/api/reservations";
